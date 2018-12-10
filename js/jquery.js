1eac806fc1b771a80f3d3d1c6cb41642c2879ed2
siegesailor <siegesailor@gmail.com>
2018-12-10 10:30:11 +0800

    RWD and Animation

--- js/jquery.js
+++ js/jquery.js
@@ -2,9 +2,17 @@ $(document).ready(function () {
     // Very beginning alert for looks for a job.
     $('.alert').hide();
     $('.alert').delay(750).fadeIn();
-    $('.alert').delay(10000).fadeOut();
-    $('.header .alert .fas').click(function () {
+    $('.header .alert i').click(function () {
         $(this).parent().fadeOut();
+    });
+
+    // Function for popup windows.
+    $('.mail').hide();
+    $('#tomail').click(function () {
+        $('.mail').fadeIn();
+    });
+    $('.mail .text i').click(function () {
+        $(this).parent().parent().fadeOut();
     });
 
     // Disable buttons for Front-End skills section.
@@ -19,7 +27,7 @@ $(document).ready(function () {
 
     // Diesable buttons for Digital Marketing skills section.
     $('.digital-marketing-skills li a').click(function (event) {
-        event.preventDefault(); 
+        event.preventDefault();
     });
 
     // Plugin / Masonry
@@ -35,29 +43,165 @@ $(document).ready(function () {
         $grid.masonry();
     });
 
+    // Plugin / Swiper
+    // http://idangero.us/swiper/get-started/
+    var swiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'progressbar',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 
-    // 進度條的顯示與否設定
-    // var showskillsection = true;
-    // $(window).scroll(function (event) {
-    //     // 定義現在視窗的滑動位置
-    //     var scrollposition = $(window).scrollTop();
-    //     // 取得現在視窗的高度
-    //     var windowheight = $(window).height();
-    //     // 取得目標y軸高度
-    //     var skillsectiontop = $('#AboutMe-E').offset().top;
 
-    //     // 目標y軸比視窗目前位置小或一樣，代表視窗目前位置在目標下方
-    //     // 因為預期是滾到就會有效果，因此把一半的視窗高度加在視窗目前位置，
-    //     // 以提早半個視窗
-    //     if (skillsectiontop <= (scrollposition + windowheight / 2) && showskillsection) {
-    //         // 對每個進度條的data數值定義
-    //         $('#skillsection .progress-bar').each(function () {
-    //             var thisvalue = $(this).data('progress');
-    //             // 進度條數值動作
-    //             $(this).css('width', thisvalue + '%');
-    //             // 進度條的顯示與否設定，開關，避免重複設定寬度
-    //             showskillsection = false;
-    //         });
-    //     }
+    // About Me / Experience / Progress-bar
+    var showskillsection = true;
+    $(window).scroll(function (event) {
+        var scrollposition = $(window).scrollTop();
+        var windowheight = $(window).height();
+        var skillsectiontop = $('#AboutMe-Experience').offset().top;
+        if (skillsectiontop <= (scrollposition + windowheight / 2) && showskillsection) {
+            $('#AboutMe-Experience .progress-bar').each(function () {
+                var thisvalue = $(this).data('progress');
+                $(this).css('width', thisvalue + '%');
+                showskillsection = false;
+            });
+        }
+    });
+
+    // Scroll Top Button
+    $('#button-animate-scrolltop').click(function (event) {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+    // Scroll Down Button
+    $('.scroll-down a').click(function () {
+        event.preventDefault();
+        var target = $(this).attr('href');
+        var targetposition = $(target).offset().top;
+        $('html,body').animate({
+            scrollTop: targetposition
+        }, 1000);
+    });
+
+    // Menu Scroll
+    $('.navbar ul li a').click(function () {
+        event.preventDefault();
+        var target = $(this).attr('href');
+        var targetposition = $(target).offset().top;
+        $('html,body').animate({
+            scrollTop: targetposition
+        }, 1000);
+    });
+
+    // Mobile Menu Scroll
+    $('.navbar-mobile ul li a').click(function () {
+        event.preventDefault();
+        var target = $(this).attr('href');
+        var targetposition = $(target).offset().top;
+        $('html,body').animate({
+            scrollTop: targetposition
+        }, 1000);
+    });
+
+    // Menu Scroll Hover
+    $(window).scroll(function (event) {
+        var scrollposition = $(window).scrollTop();
+
+        $('.navbar ul li a').each(function () {
+            var target = $(this).attr('href');
+            var targetposition = $(target).offset().top;
+            var targetheight = $(target).outerHeight();
+            if (targetposition - 1 <= scrollposition && (targetposition - 1 + targetheight) > scrollposition) {
+                $('.navbar ul li a').removeClass('.navbar-active');
+                $(this).addClass('navbar-active');
+            } else {
+                $(this).removeClass('navbar-active');
+            }
+        });
+    });
+
+    // Alert Scroll
+    $('.alert a').click(function () {
+        event.preventDefault();
+        var target = $(this).attr('href');
+        var targetposition = $(target).offset().top;
+        $('html,body').animate({
+            scrollTop: targetposition
+        }, 1000);
+        $('.alert').fadeOut();
+    });
+
+    // Achievement
+    // $('.achievement p').each(function () {
+    //     $(this).hide();
     // });
+    // $(window).scroll(function (event) {
+    //     var scrollposition = $(window).scrollTop();
+
+    //     $('.achievement p').each(function () {
+    //         var targetposition = $(this).offset().top;
+    //         var targetheight = $(this).outerHeight();
+    //         if (targetposition <= scrollposition && (targetposition + targetheight) > scrollposition) {
+    //             $(this).fadeIn();
+    //             console.log("dsa");
+    //         }
+    //     });
+    // });
+
+    // Menu Animate
+    $('#navbar-showmenu').click(function () {
+        $('#navbar-showmenu').toggleClass('pulse animated');
+    }, function () {
+        $('#navbar-showmenu').toggleClass('pulse animated');
+    });
+    $('#navbar-showmenu-back').click(function () {
+        $('#navbar-showmenu-back').toggleClass('pulse animated');
+    }, function () {
+        $('#navbar-showmenu-back').toggleClass('pulse animated');
+    });
+    // Menu Mobile Switch
+    $('#navbar-showmenu').click(function (event) {
+        event.preventDefault();
+        $('.navbar-mobile').toggleClass('navbar-mobile-active');
+        $('#navbar-showmenu').toggle();
+        $('#navbar-showmenu-back').toggle();
+    });
+    $('#navbar-showmenu-back').click(function (event) {
+        event.preventDefault();
+        $('.navbar-mobile').toggleClass('navbar-mobile-active');
+        $('#navbar-showmenu').toggle();
+        $('#navbar-showmenu-back').toggle();
+    });
+
+    // Plugin / Animate Number
+    // https://github.com/aishek/jquery-animateNumber
+    // animateNumber accepts same arguments, as animate does
+    // it adds only 'number' and 'numberStep' params
+    var shownumbers = true;
+    $(window).scroll(function (event) {
+        var scrollposition = $(window).scrollTop();
+        var windowheight = $(window).height();
+        var insighttop = $('.insight').offset().top;
+        if (insighttop <= (scrollposition + windowheight / 2) && shownumbers) {
+            $('#pageviewsnumber').animateNumber({
+                number: 264,
+            }, 1800);
+            $('#pagelikesnumber').animateNumber({
+                number: 3726,
+            }, 1800);
+            $('#pagereachnumber').animateNumber({
+                number: 19695,
+            }, 1800);
+            $('#postengagementsnumber').animateNumber({
+                number: 1115,
+            }, 1800);
+            shownumbers = false;
+        }
+    });
 });
