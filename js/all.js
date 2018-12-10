2977b56c74ede4106a17590767b8af6750e2ff75
siegesailor <siegesailor@gmail.com>
2018-12-10 12:18:27 +0800

    Disable Print Button

--- js/all.js
+++ js/all.js
@@ -3,3 +3,9 @@ document.querySelector('.header').style.height = window.innerHeight+"px"
 window.onresize = function(){
    document.querySelector('.header').style.height = window.innerHeight+"px"
 }
+
+// Print
+document.querySelector('#print').onclick = function (event) {
+   event.preventDefault();
+   window.print();
+}
